8ae641c1e4dd7e252189469ac86554fdc1ee5acd
bcast201 <bcast201@gmail.com>
2020-08-22 15:28:10 -0500

    workshop 3

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -27,6 +27,8 @@ function RenderCampsite (props) {
 
     const recognizeDrag = ({dx}) => (dx < -200) ? true : false;
 
+    const recognizeComment = ({dx}) => (dx > 200) ? true : false;
+
     const panResponder = PanResponder.create({
         onStartShouldSetPanResponder: () => true,
         onPanResponderGrant: () => {
@@ -53,8 +55,13 @@ function RenderCampsite (props) {
                     ],
                     { cancelable: false }
                 );
+                return true;
             }
-            return true;
+            else if (
+                recognizeComment(gestureState)
+            ) {props.onShowModal()};
+            return true
+
         }
     });
 
